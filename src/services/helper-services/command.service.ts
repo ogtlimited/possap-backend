bb93cc299a8ed7044ff065e06c278eeeacc35b0c
Sir Abubakar <60545690+Abubakar-Abdulwahab@users.noreply.github.com>
2022-04-05 10:28:26 +0100

    error

--- src/services/helper-services/command.service.ts
+++ src/services/helper-services/command.service.ts
@@ -25,7 +25,7 @@ class CommandService extends Repository<CommandEntity> {
   }
 
   public async createCommand(commandData: any): Promise<any> {
-    const createCommandData: any = await CommandEntity.create(commandData);
+    const createCommandData: any = await CommandEntity.create(commandData).save();
 
     return createCommandData;
   }
