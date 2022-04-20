76fe698e3203991ed2c9340925e3b916ac2cbc20
Sir Abubakar <60545690+Abubakar-Abdulwahab@users.noreply.github.com>
2022-04-20 08:04:55 +0100

    officer updates

--- src/server.ts
+++ src/server.ts
@@ -6,9 +6,10 @@ import IndexRoute from '@routes/index.route';
 import UsersRoute from '@routes/users.route';
 import validateEnv from '@utils/validateEnv';
 import OfficersRoute from './routes/officers.route';
+import HelperRoute from './routes/helper.routes';
 
 validateEnv();
 
-const app = new App([new IndexRoute(), new UsersRoute(), new AuthRoute(), new OfficersRoute()]);
+const app = new App([new IndexRoute(), new UsersRoute(), new AuthRoute(), new OfficersRoute(), new HelperRoute()]);
 
 app.listen();
