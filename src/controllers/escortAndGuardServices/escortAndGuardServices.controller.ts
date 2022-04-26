a39a823a1d5681fd7ac4c5f6fd5edeb7d1bc188b
Nnamdi ogundu <59602267+unicorncodak@users.noreply.github.com>
2022-04-26 15:30:03 +0100

    eag

--- src/controllers/escortAndGuardServices/escortAndGuardServices.controller.ts
+++ src/controllers/escortAndGuardServices/escortAndGuardServices.controller.ts
@@ -2,6 +2,9 @@ import { NextFunction, Request, Response } from 'express';
 import { CreateEscortAndGuardServiceDto } from '@dtos/escortAndGuardService/escortAndGuardService.dto';
 import { EscortAndGuardServiceApplication } from '@interfaces/EscortAndGuardServiceApplication/EscortAndGuardServiceApplication.interface';
 import EscortAndGuardServiceApplicationService from '@services/escortAndGuardServiceApplication/escortAndGuardServiceApplication.service';
+import axios from 'axios'
+var fs = require('fs');
+const util = require('util')
 
 class EscortAndGuardServiceController {
   public EscortAndGuardService = new EscortAndGuardServiceApplicationService();
@@ -47,6 +50,71 @@ class EscortAndGuardServiceController {
     } catch (error) {
       next(error);
     }
+  };
+
+  public getFetchPoliceData = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    axios.get('https://api.npprm.net/commandcategory/POSSAP/MDc5YzIyZTM1ZDAxNzlkYzVkOTViYmUwYTJkMjgxN2RkNmNjMzJhZjQzZmYxNzk2ZWY3OTA3ZWFmYjg5ZmIxMQ==/1')
+    .then(function (response) {
+      const writeStream = fs.createWriteStream("./eag.json");
+      response.data.pipe(writeStream);
+      res.status(200).json({ data: response.data, message: 'data' });
+    })
+    .catch(function (error) {
+      next(error);
+    })
+  }
+
+  public getCategory = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+      const eagId = Number(req.params.id);
+      const deleteEAGData: EscortAndGuardServiceApplication = await this.EscortAndGuardService.deleteUser(eagId);
+
+      res.status(200).json({ data: deleteEAGData, message: 'deleted' });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  public getPoliceData = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+      var obj = JSON.parse(fs.readFileSync("./eag.json", 'utf8'));
+      const d = this.filterPoliceData(obj.ResponseObject.ReportRecords, "STATE COMMAND")
+      let a = this.groupBy(d[0].sub, "StateCode")
+      if(req.query.statecode && req.query.statecode !== ""){
+        let stateCode = req.query.statecode
+        a = a[`${stateCode}`]
+      }
+      res.status(200).json({ data: a, message: 'police data' });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  public getTacticalSquad = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+      const tacticalSquad = {}
+      var obj = JSON.parse(fs.readFileSync("./eag.json", 'utf8'));
+      const d = this.filterPoliceData(obj.ResponseObject.ReportRecords, "FORCE HEADQUARTERS") 
+      const a = this.groupBy(d[0].sub, "Name")
+      const acceptedTacticalUnit = ["DOPS FHQ ABUJA", "DRP FHQ ABUJA"]
+      acceptedTacticalUnit.map(e => {
+        tacticalSquad[e] = a[e]
+      })
+      res.status(200).json({ data: tacticalSquad, message: 'police data' });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  public groupBy (xs, key) {
+    return xs.reduce(function(rv, x) {
+      (rv[x[key]] = rv[x[key]] || []).push(x);
+      return rv;
+    }, {});
+  };
+
+  public filterPoliceData (data, filterBY) {
+    return data.filter(a => a.name === filterBY)
   };
 }
 
---
+++ src/entities/EscortAndGuardService/ServiceCategory.entity.ts
@@ -0,0 +1,22 @@
+import { IsNotEmpty } from 'class-validator';
+import { BaseEntity, Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+import { ServiceCategory } from '@interfaces/EscortAndGuardServiceApplication/ServiceCategory.interface';
+
+
+@Entity()
+export class ServiceCategoryEntity extends BaseEntity implements ServiceCategory {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  @IsNotEmpty()
+  categoryName: string;
+
+  @Column()
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @Column()
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
