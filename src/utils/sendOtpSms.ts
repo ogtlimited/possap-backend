acfc240ce6dfb587eda890923eafe38a8570d3ce
Sir Abubakar <60545690+Abubakar-Abdulwahab@users.noreply.github.com>
2022-08-25 10:58:03 +0100

    main

--- src/utils/sendOtpSms.ts
+++ src/utils/sendOtpSms.ts
@@ -1,3 +1,12 @@
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
 export const sendOtpSMS = (obj) => {
  /// code
+ client.messages
+  .create({
+     body: obj.body,
+     from: 'possap.gov.ng',
+     to: obj.to
+   })
 }
