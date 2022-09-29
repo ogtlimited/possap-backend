f5f8cbcaaa14f4a3f79e9837efdf354b6d65e18a
Gaiya M. Obed <95748043+gaiyaobed@users.noreply.github.com>
2022-09-29 16:42:24 +0100

    Feat: Files upload to S3

--- src/controllers/helper-controller/helper.controller.ts
+++ src/controllers/helper-controller/helper.controller.ts
@@ -1,6 +1,12 @@
 import { NextFunction, Request, Response } from 'express';
 const { createHash } = require('crypto');
 import axios from 'axios';
+import { HttpException } from '@exceptions/HttpException';
+import { S3 } from 'aws-sdk';
+import { v4 as uuidv4 } from 'uuid';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 class HelperController {
   public verifyNIN = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
@@ -42,6 +48,72 @@ class HelperController {
     } catch (error) {
       console.log(error);
       next(error);
+    }
+  };
+
+  /*
+   * AWS Configuration
+   * */
+  private static async awsS3() {
+    return new S3({
+      accessKeyId: process.env['AWS_ACCESS_KEY_ID'],
+      secretAccessKey: process.env['AWS_SECRET_ACCESS_KEY'],
+      region: process.env['AWS_REGION'],
+    });
+  }
+
+  // Upload files
+  public uploadMedia = async (req: Request | any, res: Response, next: NextFunction) => {
+    const files = req.files;
+
+    if (!files) {
+      throw new HttpException(400, 'File is required');
+    }
+
+    try {
+      const awsS3Upload = await HelperController.awsS3();
+
+      const allFilesUpload = files.map(item =>
+        awsS3Upload
+          .upload({
+            Bucket: process.env['AWS_S3_BUCKET'],
+            Body: item.buffer,
+            Key: String(`${process.env['BUCKET_FOLDER_NAME']}/${Date.now()}-${uuidv4()}.${item.mimetype.split('/').pop()}`),
+            ContentType: 'image/jpg',
+            ACL: 'public-read',
+          })
+          .promise(),
+      );
+      const responses = await Promise.all(allFilesUpload);
+      return res.status(200).json({ status: 'Success', statusCode: 200, message: 'Uploaded successfully', data: responses });
+    } catch (err) {
+      console.log(err);
+      next(err);
+    }
+  };
+
+  //  Sent otp
+  public sendOtp = async (req: Request | any, res: Response, next: NextFunction) => {};
+
+  //  One time use
+  public createBucket = async (req: Request | any, res: Response, next: NextFunction) => {
+    const BUCKET_NAME = process.env['AWS_S3_BUCKET'];
+    try {
+      const createBucket = await HelperController.awsS3();
+
+      const params = {
+        Bucket: BUCKET_NAME,
+        CreateBucketConfiguration: {
+          LocationConstraint: process.env.AWS_REGION,
+        },
+      };
+
+      createBucket.createBucket(params, function (err, data) {
+        return res.status(200).json({ data: data.Location });
+      });
+    } catch (err) {
+      console.log(err);
+      next(err);
     }
   };
 }
--- src/routes/helper.routes.ts
+++ src/routes/helper.routes.ts
@@ -1,6 +1,9 @@
 import { Router } from 'express';
 import { Routes } from '@interfaces/routes.interface';
 import HelperController from '@/controllers/helper-controller/helper.controller';
+import multer from 'multer';
+
+const upload = multer();
 
 class HelperRoute implements Routes {
   public path = '/helper';
@@ -15,6 +18,9 @@ class HelperRoute implements Routes {
     console.log('first');
     this.router.get(`${this.path}/verifyNIN`, this.helperController.verifyNIN);
     this.router.post(`${this.path}/verifyAPNumber`, this.helperController.verifyAPNumber);
+    this.router.post(`${this.path}/uploadMedia`, upload.array('documents', 2), this.helperController.uploadMedia);
+    this.router.post(`${this.path}/sendOtp`, this.helperController.sendOtp);
+    // this.router.post(`${this.path}/createBucket`, this.helperController.createBucket);
   }
 }
 
