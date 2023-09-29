84a22dd03cc560d59d5af56b0d303f322274b68b
Sir Abubakar <60545690+Abubakar-Abdulwahab@users.noreply.github.com>
2023-09-29 09:54:59 +0100

    updates

--- src/controllers/helper-controller/CBSController.ts
+++ src/controllers/helper-controller/CBSController.ts
@@ -10,7 +10,7 @@ import path from 'path';
 const FormData = require('form-data');
 dotenv.config();
 const filestorage = path.join(__dirname, '../..', 'uploads/');
-console.log(filestorage);
+
 class CBSController {
   public ExtractRequest = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     let fileName = '';
@@ -49,8 +49,6 @@ class CBSController {
       if (helpers.hashmessage) {
         config.headers[helpers.hashField] = HMAC256Hash(helpers.clientSecret, helpers.hashmessage);
       }
-      console.log('------', config.headers);
-      console.log(fileName, 'FILENAME');
       const result = await axios.request(config);
       if (result.data) {
         removeFile(fileName);
@@ -69,7 +67,6 @@ class CBSController {
     let passportphotographfile = '';
     let intpassportdatapagefile = '';
     let proofOfResidenceFile = '';
-    console.log(req.body.requestObject);
     try {
       const { body, headers, helpers } = req.body.requestObject;
       const data = new FormData();
@@ -103,19 +100,16 @@ class CBSController {
           }
         });
       }
-      console.log(data);
       config.data = data;
       config.headers = {
         ...headers,
         ...data.getHeaders(),
       };
-      console.log(HMAC256Hash(helpers.clientSecret, helpers.hashmessage));
       if (helpers.hashmessage) {
         config.headers[helpers.hashField] = HMAC256Hash(helpers.clientSecret, helpers.hashmessage);
       }
-      // console.log(intpassportdatapagefile, passportphotographfile, 'FILENAME');
       const result = await axios.request(config);
-      // console.log(result);
+
       if (result.data) {
         removeFile(passportphotographfile);
         removeFile(passportphotographfile);
@@ -139,7 +133,6 @@ class CBSController {
     }
   };
   public EGSRequest = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    console.log(req.body.requestObject);
     try {
       const { body, headers, helpers } = req.body.requestObject;
       const data = new FormData();
@@ -157,18 +150,15 @@ class CBSController {
           }
         });
       }
-      // console.log(data);
       config.data = data;
       config.headers = {
         ...headers,
         ...data.getHeaders(),
       };
-      // console.log(config);
       if (helpers.hashmessage) {
         config.headers[helpers.hashField] = HMAC256Hash(helpers.clientSecret, helpers.hashmessage);
       }
       const result = await axios.request(config);
-      console.log('result');
       if (result.data) {
         res.status(200).json({ data: result.data });
       } else {
@@ -182,11 +172,9 @@ class CBSController {
 
   public tempUpload = async (req: any, res: Response, next: NextFunction): Promise<void> => {
     try {
-      console.log(req.file);
       if (req.file) {
         const { filename } = req.file;
         //getFile(req.file.filename);
-        console.log(req.file);
         res.status(200).json({ status: 'Success', statusCode: 200, message: 'Files uploaded successfully', data: filename });
       } else {
         res.status(400).json({ message: 'error uploading file' });
@@ -199,7 +187,6 @@ class CBSController {
   };
 
   public FetchRequest = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    // console.log(req.body.requestObject);
     try {
       const { headers, helpers, body } = req.body.requestObject;
       const config: AxiosRequestConfig = {
@@ -213,14 +200,11 @@ class CBSController {
       if (body) {
         config.data = body;
       }
-      // console.log(config);
       if (helpers.hashmessage) {
         config.headers[helpers.hashField] = HMAC256Hash(helpers.clientSecret, helpers.hashmessage);
       }
-      // console.log(config, headers, helpers);
-      console.log(config);
       const result = await axios.request(config);
-      console.log(result);
+
       if (result.data) {
         res.status(200).json({ data: result.data });
       } else {
